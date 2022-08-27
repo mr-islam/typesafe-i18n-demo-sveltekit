dc867c26f4f79e4c60a9117d9c3f04f8abc67a92
Naved Islam <navedcoded@gmail.com>
2022-08-27 12:14:48 +0300

    setup doc dir

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
+<script context="module">
+	import { onMount, afterUpdate } from 'svelte'
+</script>
+
 <script lang="ts">
 	import Header from '$lib/Header.svelte'
-	import { setLocale } from '$i18n/i18n-svelte'
+	import { setLocale, locale } from '$i18n/i18n-svelte'
 	import type { LayoutData } from './$types'
 
 	export let data: LayoutData
 
 	setLocale(data.locale)
+	onMount(() => {
+		document.dir = $locale === 'ar' ? 'rtl' : 'ltr'
+	})
+	afterUpdate(() => {
+		document.dir = $locale === 'ar' ? 'rtl' : 'ltr'
+	})
 </script>
 
 <Header />
