32d69ac3a53176884e35c44a29c53e13c47be17b
Naved Islam <navedcoded@gmail.com>
2022-08-27 11:51:36 +0300

    start header redesign

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,8 +5,73 @@
 
 <header>
 	<a href="/{$locale}">
-		<h1>typesafe-i18n</h1>
+		<img alt="Logo of Arabic calligraphy" id="logo" src="/logo-new.jpg" />
 	</a>
 
 	<LocaleSwitcher />
 </header>
+
+<style>
+	header {
+	  background-color: var(--pure-white);
+	  display: flex;
+	  flex-direction: row;
+	  align-items: center;
+	  justify-content: space-between;
+	  border-bottom: 4px solid var(--primary-color);
+	  left: 30vw;
+	  padding: 10px 0;
+	  z-index: 5;
+	}
+	img#logo {
+	  border: 1px black solid;
+	  border-radius: 360px;
+	  max-width: 5vw;
+	  margin: 5px 12px 2px 5px;
+	  margin-left: 70px;
+	}
+	div > div > a {
+	  background-color: var(--accent-color);
+	  color: var(--primary-color);
+	  padding: 5px;
+	  margin: 5px;
+	  border-radius: 4px;
+	}
+	
+	div.flex-column {
+	  margin: 20px 0 0 0;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	}
+	@media only screen and (max-width: 600px) {
+	  img#logo {
+		max-width: 8vw;
+		margin: 10px;
+	  }
+	  header {
+		position: relative;
+		flex-direction: column;
+		left: 0;
+		min-width: 100%;
+		border-radius: 0px;
+		top: 0px;
+		border: 0px;
+		padding-top: 10px;
+	  }
+	  h1 {
+		font-size: 0.8rem;
+	  }
+	  div.flex-column {
+		flex-direction: column;
+	  }
+	}
+	select {
+	  margin: 10px;
+	}
+	h1 {
+	  font-size: 1.5rem;
+	  margin: 0;
+	}
+  </style>
+  
--- src/lib/LocaleSwitcher.svelte
+++ src/lib/LocaleSwitcher.svelte
@@ -3,7 +3,6 @@
 	import { page } from '$app/stores'
 	import { setLocale, locale } from '$i18n/i18n-svelte'
 	import type { Locales } from '$i18n/i18n-types'
-	import { locales } from '$i18n/i18n-util'
 	import { loadLocaleAsync } from '$i18n/i18n-util.async'
 	import { replaceLocaleInUrl } from '../utils'
 
@@ -38,12 +37,72 @@
 
 <svelte:window on:popstate={handlePopStateEvent} />
 
-<ul>
-	{#each locales as l}
-		<li>
-			<button type="button" class:active={l === $locale} on:click={() => switchLocale(l)}>
-				{l}
-			</button>
-		</li>
-	{/each}
-</ul>
+<select id="select-lang" on:change={() => switchLocale(document.getElementById("select-lang").selectedOptions[0].value)}>
+	<option value={"it"} class:active={$locale == "ar"}>العربية</option>
+	<option value={"en"} class:active={$locale == "ar"}>English</option>
+</select>
+
+<style>
+	header {
+	  background-color: var(--pure-white);
+	  display: flex;
+	  flex-direction: row;
+	  align-items: center;
+	  justify-content: space-between;
+	  border-bottom: 4px solid var(--primary-color);
+	  left: 30vw;
+	  padding: 10px 0;
+	  z-index: 5;
+	}
+	img#logo {
+	  border: 1px black solid;
+	  border-radius: 360px;
+	  max-width: 5vw;
+	  margin: 5px 12px 2px 5px;
+	  margin-left: 70px;
+	}
+	div > div > a {
+	  background-color: var(--accent-color);
+	  color: var(--primary-color);
+	  padding: 5px;
+	  margin: 5px;
+	  border-radius: 4px;
+	}
+	
+	div.flex-column {
+	  margin: 20px 0 0 0;
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	}
+	@media only screen and (max-width: 600px) {
+	  img#logo {
+		max-width: 8vw;
+		margin: 10px;
+	  }
+	  header {
+		position: relative;
+		flex-direction: column;
+		left: 0;
+		min-width: 100%;
+		border-radius: 0px;
+		top: 0px;
+		border: 0px;
+		padding-top: 10px;
+	  }
+	  h1 {
+		font-size: 0.8rem;
+	  }
+	  div.flex-column {
+		flex-direction: column;
+	  }
+	}
+	select {
+	  margin: 10px;
+	}
+	h1 {
+	  font-size: 1.5rem;
+	  margin: 0;
+	}
+  </style>
+  
