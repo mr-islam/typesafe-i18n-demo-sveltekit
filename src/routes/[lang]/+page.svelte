595a6bf8f5140c0a2fc55792f0ce98281a122508
Naved Islam <navedcoded@gmail.com>
2022-08-28 21:44:18 +0300

    capitalize for CF error

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -2,9 +2,9 @@
 	import LL from '$i18n/i18n-svelte'
 	import Gallery from '$lib/Gallery.svelte'
 	import IntroBullets from '$lib/IntroBullets.svelte'
-	import Courses from '$lib/courses.svelte'
-	import Articles from '$lib/articles.svelte'
-	import Goals from '$lib/goals.svelte'
+	import Courses from '$lib/Courses.svelte'
+	import Articles from '$lib/Articles.svelte'
+	import Goals from '$lib/Goals.svelte'
 
 	console.log($LL.title)
 </script>
