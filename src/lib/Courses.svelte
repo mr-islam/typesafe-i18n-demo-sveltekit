23e2962bb7d150fd7d852b71cc536b2852c9a3f4
Naved Islam <navedcoded@gmail.com>
2022-09-22 18:06:40 +0300

    redo edge label as independent nodes

--- src/lib/Courses.svelte
+++ src/lib/Courses.svelte
@@ -22,7 +22,18 @@
 		},
 		{
 			id: 2,
-			position: { x: 50, y: 100 },
+			position: { x: 250, y: 100 },
+			data: { label: 'علم التوحيد' },
+			width: 80,
+			height: 40,
+			bgColor: 'skyblue',
+			sourcePosition: 'left',
+			targetPosition: 'right',
+			borderColor: 'skyblue'
+		},
+		{
+			id: 3,
+			position: { x: 50, y: 75 },
 			data: {
 				label: 'معرفة العقائد الواجبة والمتسحيلة والجائزة في حق اللع تعالى ورسله عليهم الصلاة والسلام والإيمان بالسمعيات',
 			},
@@ -32,41 +43,93 @@
 			targetPosition: 'right',
 		},
 		{
-			id: 3,
-			position: { x: 50, y: 225 },
-			data: { label: 'إتقان تلاوة القرآن وترتيله' },
-			width: 175,
+			id: 4,
+			position: { x: 250, y: 200 },
+			data: { label: 'علم التجويد' },
+			width: 80,
+			height: 40,
+			bgColor: 'skyblue',
+			sourcePosition: 'left',
+			targetPosition: 'right',
+			borderColor: 'skyblue'
+		},
+		{
+			id: 5,
+			position: { x: 100, y: 200 },
+			data: {
+				label: 'إتقان تلاوة القرآن وترتيله',
+			},
+			width: 125,
 			height: 40,
 			bgColor: 'white',
 			targetPosition: 'right',
 		},
 		{
-			id: 4,
-			position: { x: 50, y: 280 },
-			data: { label: 'الإتيان بالعبادات غلى الوجه الشرعي الصحيح' },
+			id: 6,
+			position: { x: 250, y: 275 },
+			data: { label: 'علم العبادات' },
+			width: 80,
+			height: 40,
+			bgColor: 'skyblue',
+			sourcePosition: 'left',
+			targetPosition: 'right',
+			borderColor: 'skyblue'
+		},
+		{
+			id: 7,
+			position: { x: 50, y: 275 },
+			data: {
+				label: 'الإتيان بالعبادات على الوجه الشرعي الصحيح',
+			},
 			width: 175,
 			height: 50,
 			bgColor: 'white',
 			targetPosition: 'right',
 		},
 		{
-			id: 5,
-			position: { x: 50, y: 345 },
-			data: { label: 'إجراء المعاملات الشاملة للبيوع، وأحكام الأسرة على الوجه الذي شرعه الله تعالى' },
+			id: 8,
+			position: { x: 250, y: 350 },
+			data: { label: 'علم المعاملات' },
+			width: 80,
+			height: 40,
+			bgColor: 'skyblue',
+			sourcePosition: 'left',
+			targetPosition: 'right',
+			borderColor: 'skyblue'
+		},
+		{
+			id: 9,
+			position: { x: 50, y: 350 },
+			data: {
+				label: 'إجراء المعاملات الشاملة للبيوع، وأحكام الأسرة على الوجه الذي شرعه الله تعالى',
+			},
 			width: 175,
 			height: 70,
 			bgColor: 'white',
 			targetPosition: 'right',
 		},
 		{
-			id: 6,
-			position: { x: 50, y: 425 },
-			data: { label: 'إصلاح القلب وتهذيب النفس' },
-			width: 175,
+			id: 10,
+			position: { x: 250, y: 450 },
+			data: { label: 'علم التصوف' },
+			width: 80,
 			height: 40,
+			bgColor: 'skyblue',
+			sourcePosition: 'left',
+			targetPosition: 'right',
+			borderColor: 'skyblue'
+		},
+		{
+			id: 11,
+			position: { x: 50, y: 450 },
+			data: {
+				label: 'إصلاح القلب وتهذيب النفس',
+			},
+			width: 175,
+			height: 70,
 			bgColor: 'white',
 			targetPosition: 'right',
-		},
+		}		
 	]
 
 	const initialEdges = [
@@ -74,60 +137,80 @@
 			id: 'e1-2',
 			source: 1,
 			target: 2,
-			label: 'علم التوحيد',
-			labelBgColor: 'skyblue',
-			labelTextColor: 'black',
 			noHandle: true,
-			arrow: true,
-			animate: true,
+			arrow: false,
 			edgeColor: 'black'
 		},
 		{
-			id: 'e1-3',
-			source: 1,
+			id: 'e2-3',
+			source: 2,
 			target: 3,
-			label: 'علم التجويد',
-			labelBgColor: 'skyblue',
-			labelTextColor: 'black',
 			noHandle: true,
 			arrow: true,
-			animate: false,
 			edgeColor: 'black'
 		},
 		{
 			id: 'e1-4',
 			source: 1,
 			target: 4,
-			label: 'علم العبادات',
-			labelBgColor: 'skyblue',
-			labelTextColor: 'black',
 			noHandle: true,
-			arrow: true,
-			animate: false,
+			arrow: false,
 			edgeColor: 'black'
 		},
 		{
-			id: 'e1-5',
-			source: 1,
+			id: 'e4-5',
+			source: 4,
 			target: 5,
-			label: 'علم المعاملات',
-			labelBgColor: 'skyblue',
-			labelTextColor: 'black',
 			noHandle: true,
 			arrow: true,
-			animate: false,
 			edgeColor: 'black'
 		},
 		{
 			id: 'e1-6',
 			source: 1,
 			target: 6,
-			label: 'علم التصوف',
-			labelBgColor: 'skyblue',
-			labelTextColor: 'black',
+			noHandle: true,
+			arrow: false,
+			edgeColor: 'black'
+		},
+		{
+			id: 'e6-7',
+			source: 6,
+			target: 7,
 			noHandle: true,
 			arrow: true,
-			animate: false,
+			edgeColor: 'black'
+		},
+		{
+			id: 'e1-8',
+			source: 1,
+			target: 8,
+			noHandle: true,
+			arrow: false,
+			edgeColor: 'black'
+		},
+		{
+			id: 'e8-9',
+			source: 8,
+			target: 9,
+			noHandle: true,
+			arrow: true,
+			edgeColor: 'black'
+		},
+		{
+			id: 'e1-10',
+			source: 1,
+			target: 10,
+			noHandle: true,
+			arrow: false,
+			edgeColor: 'black'
+		},
+		{
+			id: 'e10-11',
+			source: 10,
+			target: 11,
+			noHandle: true,
+			arrow: true,
 			edgeColor: 'black'
 		},
 	]
@@ -135,7 +218,7 @@
 
 <h1>علوم الشريعة والحقيقة</h1>
 <div>
-	<Svelvet width={500} height={1000} nodes={initialNodes} edges={initialEdges} movement={true} background />
+	<Svelvet width={500} height={1000} nodes={initialNodes} edges={initialEdges} movement={true} />
 </div>
 
 
