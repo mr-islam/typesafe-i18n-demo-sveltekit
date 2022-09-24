63892d4cf0e90aab0c35c6cedfaab7196340f08a
Naved Islam <navedcoded@gmail.com>
2022-09-24 14:33:39 +0300

    locale based style for texts

--- src/lib/IntroBullets.svelte
+++ src/lib/IntroBullets.svelte
@@ -1,11 +1,25 @@
-<script context="module">
+<script>
 	import LL, { locale } from '$i18n/i18n-svelte'
+	import { onMount, afterUpdate } from 'svelte'
+
+	let p_elements // This below: for custom styles (fonts mainly) based on language
+	onMount(() => {
+		p_elements = document.querySelectorAll('.intro-bullet')
+		for (const p_element of p_elements) {
+			p_element.classList.add($locale === 'ar' ? 'rtl' : 'ltr')
+		}
+	})
+	afterUpdate(() => {
+		for (const p_element of p_elements) {
+			p_element.classList.add($locale === 'ar' ? 'rtl' : 'ltr')
+		}
+	})
 </script>
 
 <div id="flex">
 	<a class="box" id="box-1" href="/{$locale}/goals">
 		<div>
-			<p>
+			<p class="intro-bullet">
 				{$LL.welcome.mission()}
 			</p>
 		</div>
@@ -13,14 +27,16 @@
 
 	<a class="box" id="box-4" href="/{$locale}/articles">
 		<div>
-			<p>
+			<p class="intro-bullet">
 				{$LL.welcome.online()}
 			</p>
 		</div>
 	</a>
 	<a class="box" id="box-3" href="/{$locale}/contact">
 		<div>
-			<p>{$LL.welcome.contact()}:</p>
+			<p class="intro-bullet">
+				{$LL.welcome.contact()}:
+			</p>
 		</div>
 	</a>
 
@@ -42,17 +58,24 @@
 		height: 100%;
 	}
 
-	P {
+	:global(p.rtl) {
 		color: var(--accent-color);
 		padding: 15px 10px;
-		margin: 10px;
+		margin: 10px 30px;
+		font-weight: bold;
+		font-size: 1.5rem;
+	}
+	:global(p.ltr) {
+		color: var(--accent-color);
+		padding: 15px 10px;
+		margin: 10px 30px;
+		font-size: 1.2rem;
 	}
 
 	a.box {
 		/* margin: 5px; */
 		border-radius: 12px;
 		box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-		font-size: 1.5rem;
 	}
 
 	a#box-1 {
