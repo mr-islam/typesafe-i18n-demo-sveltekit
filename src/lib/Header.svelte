45a093729a7e3da27d0cfe242c803e7a35cbef9c
Naved Islam <navedcoded@gmail.com>
2022-10-26 20:46:59 +0800

    add box shadows

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -49,6 +49,7 @@
 		border: 2px solid green;
 		padding: 8px;
 		background-color: rgb(214, 247, 214);
+		box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 	}
 	h1 {
 		font-size: 2.2rem;
@@ -100,7 +101,7 @@
 		border: 1px black solid;
 		border-radius: 360px;
 		max-width: 14vw;
-	}
+		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;	}
 	h2 {
 		margin-bottom: 0;
 	}
